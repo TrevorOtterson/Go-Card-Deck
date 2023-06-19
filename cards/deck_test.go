daef931f9312f4d66dcf98d71da99e7ef5635479
Trevor Otterson <totterson@nerdunited.com>
2023-06-19 15:53:33 -0600

    finished code created a card deck that will shuffle and save to file. Created tests for code as well

---
+++ cards/deck_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Diamonds" {
+		t.Errorf("Expected first card of Ace of Diamonds, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
